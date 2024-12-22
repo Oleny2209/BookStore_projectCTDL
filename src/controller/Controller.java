0d25ca95c6e8eb5eb3faa04d319445ac613a7d45
Oleny KleiTus <olenyDev@gmail.com>
2024-12-22 15:38:24 +0700

    upload Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,13 @@
 package controller;
 
-public class Controller {
+import view.MainFrame;
 
+public class Controller {
+    public Controller(){
+    
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new MainFrame();
+    }
 }
